880a7141afb2ac951c4c066cd190d515a31a9214
Nikita <kabanov.n.v@mail.ru>
2022-05-19 13:03:58 +0300

    added bindActionCreators, refactoring

--- src/index.js
+++ src/index.js
@@ -1,64 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
+
+import reducer from './reducer';
+import * as actions from './actions';
 
 import './index.css';
 
-const initialState = { counter: 0 };
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INC':
-            return {
-                ...state,
-                counter: state.counter + 1,
-            };
-        case 'DEC':
-            return {
-                ...state,
-                counter: state.counter - 1,
-            };
-        case 'RND':
-            return {
-                ...state,
-                counter: state.counter * action.payload,
-            };
-        case 'RESET':
-            return {
-                ...state,
-                counter: 0,
-            };
-        default:
-            return state;
-    }
-};
-
 const store = createStore(reducer);
 
-store.subscribe(() => {
-    document.getElementById('counter').textContent = store.getState().counter;
+const { dispatch, subscribe, getState } = store;
+
+subscribe(() => {
+    document.getElementById('counter').textContent = getState().counter;
 });
 
-const inc = () => ({ type: 'INC' });
-const dec = () => ({ type: 'DEC' });
-const rnd = (value) => ({ type: 'RND', payload: value });
-const reset = () => ({ type: 'RESET' });
+// const bindActionCreator =
+//     (creator, dispatch) =>
+//     (...args) => {
+//         dispatch(creator(...args));
+//     };
 
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc());
-});
+const { inc, dec, rnd, reset } = bindActionCreators(actions, dispatch);
 
-document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec());
-});
+document.getElementById('inc').addEventListener('click', inc);
 
-document.getElementById('reset').addEventListener('click', () => {
-    store.dispatch(reset());
-});
+document.getElementById('dec').addEventListener('click', dec);
+
+document.getElementById('reset').addEventListener('click', reset);
 
 document.getElementById('rnd').addEventListener('click', () => {
     const value = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(value));
+    rnd(value);
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
