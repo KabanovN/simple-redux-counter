9dddbb9367082d88a2b592b3c86daae808930cdd
Nikita <kabanov.n.v@mail.ru>
2022-05-19 11:31:19 +0300

    first stage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
-
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            {/* <div className='container'>
+                <h1 className='title'>0</h1>
+                <button onClick={inc} className='btn btn-primary'>
+                    INC
+                </button>
+                <button onClick={dec} className='btn btn-primary'>
+                    DEC
+                </button>
+                <button onClick={rnd} className='btn btn-primary'>
+                    RND
+                </button>
+                <button onClick={reset} className='btn btn-primary'>
+                    RESET
+                </button>
+            </div> */}
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
 import './index.css';
-import App from './App';
+
+const initialState = { counter: 0 };
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC':
+            return {
+                ...state,
+                counter: state.counter + 1,
+            };
+        case 'DEC':
+            return {
+                ...state,
+                counter: state.counter - 1,
+            };
+        case 'RND':
+            return {
+                ...state,
+                counter: state.counter * action.payload,
+            };
+        case 'RESET':
+            return {
+                ...state,
+                counter: 0,
+            };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+    document.getElementById('counter').textContent = store.getState().counter;
+});
+
+const inc = () => ({ type: 'INC' });
+const dec = () => ({ type: 'DEC' });
+const rnd = (value) => ({ type: 'RND', payload: value });
+const reset = () => ({ type: 'RESET' });
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec());
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+    store.dispatch(reset());
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    store.dispatch(rnd(value));
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <></>
     </React.StrictMode>
 );
